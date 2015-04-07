2011f98fe30dd121cba7620934430d9232bf2b3d
Slava <0503127085@ukr.net>
2015-04-07 09:50:25 +0300

    Bugreport send feature added; Minor translations fixes;

--- Colines3/app/src/main/java/ua/com/glady/colines3/MyApplication.java
+++ Colines3/app/src/main/java/ua/com/glady/colines3/MyApplication.java
@@ -1,7 +1,12 @@
 package ua.com.glady.colines3;
 
 import android.app.Application;
+import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * This class replaced native application since we have to handle uncaught exception in same
@@ -27,6 +32,30 @@ public class MyApplication extends Application {
     }
 
     /**
+     * @param e exception that we need to analyze
+     * @return json string with exception stacktrace and some device information
+     *  if there is an error while creating json string, just stacktrace returned
+     */
+    private String getExceptionReport(Throwable e){
+        JSONObject json = new JSONObject();
+
+        String stackTrace = Log.getStackTraceString(e);
+        try {
+            json.put("stacktrace", stackTrace);
+            json.put("brand", Build.BRAND);
+            json.put("device", Build.DEVICE);
+            json.put("model", Build.MODEL);
+            json.put("id", Build.ID);
+            json.put("version_release", Build.VERSION.RELEASE);
+            json.put("version_incremental", Build.VERSION.INCREMENTAL);
+            return json.toString();
+        } catch (JSONException e1) {
+            // something wrong, but "at least I have chicken.."
+            return stackTrace;
+        }
+    }
+
+    /**
      * This method defines how all uncaught exceptions will be handled in application
      * @param e uncaught exception
      */
@@ -36,9 +65,14 @@ public class MyApplication extends Application {
 
         Log.d("glady", "Crash:" + Log.getStackTraceString(e));
 
+        Intent intent = new Intent();
+        intent.setAction("ua.com.glady.colines3.SendExceptionActivity"); // see step 5.
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // required when starting from Application
+        intent.putExtra("exceptionReport", getExceptionReport(e));
+        startActivity(intent);
+
         // We can't do anything new with it
         System.exit(1);
     }
-
 
 }
---
+++ Colines3/app/src/main/java/ua/com/glady/colines3/SendExceptionActivity.java
@@ -0,0 +1,82 @@
+package ua.com.glady.colines3;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+
+/**
+ * This activity provides ability to send bugreport for developers
+ *
+ * Created by Slava on 19.03.2015.
+ */
+public class SendExceptionActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private static final String BUGREPORT_COLLECTING_EMAIL = "uacc.project@ukr.net";
+
+    private String exceptionReport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_send_exception);
+
+        exceptionReport = this.getIntent().getExtras().getString("exceptionReport");
+    }
+
+    /**
+     * Runs e-mail intent
+     */
+    private void sendException() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("plain/text");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{BUGREPORT_COLLECTING_EMAIL});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "UACC exception");
+        intent.putExtra(Intent.EXTRA_TEXT, exceptionReport); // do this so some email clients don't complain about empty body.
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Checks if user allows to send bugreport
+     *
+     * @return true if user allows to send bugreport
+     */
+    private boolean needToSendException() {
+        CheckBox cb = (CheckBox) this.findViewById(R.id.cbSendException);
+        return cb.isChecked();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btCloseApplication) {
+            if (needToSendException())
+                sendException();
+            this.finish();
+        }
+    }
+}
